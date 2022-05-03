f6006143f8c8600a3d94b835b4d482e5fe2da240
arielcatli <arielcatli@gmail.com>
2022-05-03 11:49:16 +0800

    [ENDPOINTS] research cost endpoints

--- herd_backend/metrics/urls.py
+++ herd_backend/metrics/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('research-cost/', views.get_research_cost, name="research_cost"),
     path('research-cost/funding', views.get_research_funding, name='research_funding'),
-    
+    path('research-cost/funding-type', views.get_research_funding_type, name='research_funding_type'),
+    path('research-cost/funding-source', views.get_research_funding_source, name="research_funding_source"),
+    path('research-cost/funding-forecast', views.get_research_funding_forecast, name="research_funding_forecast")
 ]
--- herd_backend/metrics/views.py
+++ herd_backend/metrics/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from core.research_cost import get_research_cost_table as research_cost_table
 from core.research_cost import get_research_cost_budget
+from core.research_cost import get_research_cost_funding_type as funding_type
+from core.research_cost import get_research_cost_funding_source as funding_source
+from core.research_cost import get_research_cost_budget_line as funding_forecast
 from core.utilities import makeJsonResponse, get_columns, process_for_response
 
 from django.http import JsonResponse
@@ -17,3 +20,12 @@ def get_research_cost(request):
 
 def get_research_funding(request):
     return makeJsonResponse(get_research_cost_budget())
+
+def get_research_funding_type(request):
+    return makeJsonResponse(funding_type())
+
+def get_research_funding_source(request):
+    return makeJsonResponse(funding_source())
+
+def get_research_funding_forecast(request):
+    return makeJsonResponse(funding_forecast())
