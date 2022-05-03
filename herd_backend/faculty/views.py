7de09813c8c919005278ae112bd0a49705e57d0e
arielcatli <arielcatli@gmail.com>
2022-05-03 15:33:16 +0800

    [FACULTY] Integrated faculty features

--- herd_backend/faculty/views.py
+++ herd_backend/faculty/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from core.faculty import get_faculty_profile_table as faculty_profile
 from core.faculty import get_faculty_educ_attain
 
-from core.utilities import makeJsonResponse, get_columns
+from core.utilities import makeJsonResponse, get_columns, invert_table, \
+        create_stacked_bar_chart_data
 
 ######################################
 #      FACULTY PROFILE               #
 ######################################
 
 def get_faculty_profile(request):
-    data = faculty_profile()
-    metadata = {
-        "columns": get_columns(data)
-    }
-    return makeJsonResponse(data, metadata, is_modified=True)
-
+    return JsonResponse(invert_table(faculty_profile()), safe=False)
+    
 # charts
 
 def get_faculty_education(request):
-    return makeJsonResponse(get_faculty_educ_attain())
+    return JsonResponse(create_stacked_bar_chart_data(get_faculty_educ_attain()), safe=False)
